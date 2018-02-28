d6fef4e1e8c6856d415e02733f5bc504b609f29f
Yeongseon Choe <yeongseon.choe@gmail.com>
2018-02-28 14:23:35 +0900

    Add examples

---
+++ Python2/Class/dog2.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+class Dog:
+       
+    def __init__(self, name):
+        self.name = name
+        self.tricks = []
+        
+    def add_trick(self, trick):
+        self.tricks.append(trick)
+        
+
+d = Dog('Fido')
+e = Dog('Buddy')
+d.add_trick('roll over')
+e.add_trick('play dead')
+
+print(d.tricks)
+print(e.tricks)
